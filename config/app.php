93f23a9f23bf0bdbc8652beb4e7124c464bb66fc
Tomonori Ueno <t.uepon@gmail.com>
2018-09-16 11:14:18 +0900

    Revert "Registered to Twitter API and settup completed"
    This reverts commit 06e95732adb469cdaf3e7e8b5797bef139a045ab.

--- config/app.php
+++ config/app.php
@@ -146,7 +146,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Thujohn\Twitter\TwitterServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -209,7 +208,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Twitter' => Thujohn\Twitter\Facades\Twitter::class,
+
     ],
 
 ];
